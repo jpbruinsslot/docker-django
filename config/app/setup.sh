cb14da0136d54b587b835048bf65ba2549a34373
erroneousboat <jpbruinsslot@gmail.com>
2015-01-29 14:40:23 +0100

    config: removed reference to collectstatic command

--- config/app/setup.sh
+++ config/app/setup.sh
@@ -11,4 +11,3 @@ j2 /srv/config/nginx/nginx.j2 > /etc/nginx/sites-enabled/default
 # Django setup
 #####
 python /srv/django/${DJANGO_PROJECT_NAME}/manage.py syncdb --noinput
-# python /srv/django/${DJANGO_PROJECT_NAME}/manage.py collectstatic --noinput
