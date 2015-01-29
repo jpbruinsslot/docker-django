31f69a3ce8e7712f7c82450c1cad10b4fba9a2ed
erroneousboat <jpbruinsslot@gmail.com>
2015-01-29 16:23:06 +0100

    static-files: made changes to support static files

--- config/app/setup.sh
+++ config/app/setup.sh
@@ -10,4 +10,9 @@ j2 /srv/config/nginx/nginx.j2 > /etc/nginx/sites-enabled/default
 #####
 # Django setup
 #####
+
+# Django: syncdb
 python /srv/django/${DJANGO_PROJECT_NAME}/manage.py syncdb --noinput
+
+# Django: collectstatic
+python /srv/django/${DJANGO_PROJECT_NAME}/manage.py collectstatic --noinput
