46bfca537e5558abfa65eef881ea95093c97c685
Bazyli Brzóska <bazyli.brzoska@gmail.com>
2015-11-05 21:45:01 +0100

    Support LetsEncrypt certificates out of box

--- config/nginx/start.sh
+++ config/nginx/start.sh
@@ -1,6 +1,44 @@
 #!/bin/bash
 #set -e
 
-sed -ri "s/##NGINX_SERVER_NAME##/${NGINX_SERVER_NAME:-localhost}/" /etc/nginx/conf.d/default.conf
+function replace {
+  LC_ALL=C sed -i "s/$(echo $1 | sed -e 's/\([[\/.*]\|\]\)/\\&/g')/$(echo $2 | sed -e 's/[\/&]/\\&/g')/g" $3
+}
+
+replace "##NGINX_SERVER_NAME##" "${NGINX_SERVER_NAME:-localhost}" /etc/nginx/conf.d/default.conf
+
+if [[ ! -e /srv/cert_installed ]]; then
+	# FIRST RUN WITH INVALID CERT:
+	cp /etc/nginx/conf.d/default.conf /etc/nginx/conf.d/default.conf.copy
+	replace "##CERT_PATH##" "/srv/config" /etc/nginx/conf.d/default.conf
+	
+	if [[ $LETSENCRYPT == true ]]; then
+		# REPLACE WITH LETSENCRYPT CERT:
+		mkdir -p /etc/letsencrypt/webrootauth
+		chmod 666 /etc/letsencrypt/webrootauth
+		chown www-data:www-data /etc/letsencrypt/webrootauth
+	
+		{
+			sleep 3
+			pattern="/etc/letsencrypt/live/${NGINX_SERVER_NAME}*"
+			certsPat="$pattern/fullchain.pem"
+			
+			until [[ "${certs[0]}" != "$certsPat" && ${#certs[@]} -gt 0 ]]; do
+				certs=( $certsPat )
+				sleep 5
+			done
+			echo "*** Certificate installed, reloading ..."
+			
+			directories=( $pattern )
+			
+			rm /etc/nginx/conf.d/default.conf
+			cp /etc/nginx/conf.d/default.conf.copy /etc/nginx/conf.d/default.conf
+			replace "##CERT_PATH##" "${directories[0]}" /etc/nginx/conf.d/default.conf
+			touch /srv/cert_installed
+			
+			/srv/config/reload.sh
+		} &
+	fi
+fi
 
 exec "$@"
